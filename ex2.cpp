ea21dbd11fd74298d50a0017182118e9ddc9f590
sarahlpdias <diassarah238@gmail.com>
2025-04-18 20:08:33 -0300

    exe2

---
+++ ex2.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //pra usar entrada e saída
+using namespace std; //pra não precisar colocar o std:: na frente da cada linha
+
+int main (){
+    int a, b, c ; //declara variáveis inteiras
+    cin >> a >> b >> c ; //recebe os 3 números
+    int sum = a + b + c ; //soma os 3 números
+    cout << sum; //mostra a soma dos números
+
+    return 0;
+}
