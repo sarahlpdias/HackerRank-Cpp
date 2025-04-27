4a10054cb879fd70a9d182fae712e042f2b78e96
sarahlpdias <diassarah238@gmail.com>
2025-04-27 14:56:45 -0300

    exe14

---
+++ 14 - class/ex14.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class Student {
+    private:
+        int age;
+        string first_name;
+        string last_name;
+        int standard;
+    public:
+
+        void set_age(int a) { age = a;} // set_age para gravar a idade
+        void set_first_name( string fn) {first_name = fn;}
+        void set_last_name( string ln) {last_name = ln;}
+        void set_standard(int std) {standard = std;}
+
+        int get_age() {return age;} //get_age para ler a idade
+        string get_first_name() {return first_name;}
+        string get_last_name() {return last_name;}
+        int get_standard(){return standard;}
+
+        string to_string(){
+
+            stringstream ss; //ler a entrada de forma mais estruturada
+            ss << age << "," << first_name << "," << last_name << "," << standard; // o output sai na forma que queremos e com o uso das vírgulas
+            return ss.str();
+
+        }
+
+};
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+    
+    cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+    
+    return 0;
+}
